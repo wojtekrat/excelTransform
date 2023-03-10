b6d5c0e475881f6789e7d77cde17aad79199db29
Wojtek <41506511+wojtekrat@users.noreply.github.com>
2023-03-11 00:20:20 +0100

    Add files via upload

--- folder_creation.py
+++ folder_creation.py
@@ -3,42 +3,48 @@ import shutil
 from openpyxl import load_workbook
 from tkinter import *
 from tkinter import filedialog as fd
+import datetime
 
 xlsx_path = ''
 folder_path = ''
+
 root = Tk()
 root.title("Excel manager")
-root.geometry("380x180")
+
 root.resizable(False, False)
 
-db_button = Button(root, text="Wybierz plik Excel", command=lambda: openFile())
+db_button = Button(root, text="Wybierz plik Excel", command=lambda: open_file())
 db_button.grid(row=0, column=0)
-db_name_label1 = Label(root, text=xlsx_path)
-db_name_label1.grid(row=1, column=0)
+db_name_label1 = Label(root, text='Plik Excel nie zostal wybrany')
+db_name_label1.grid(row=2, column=0)
 
 db_name_label = Label(root, text="Wpisz nazwe arkusza: ")
-db_name_label.grid(row=3, column=0)
+db_name_label.grid(row=4, column=0)
 db_name = Entry(root, width=60)
-db_name.grid(row=4, column=0)
+db_name.grid(row=6, column=0)
 
 open_button = Button(root, text="Wybierz folder z plikami", command=lambda: open_folder())
-open_button.grid(row=5, column=0)
-db_name_label2 = Label(root, text=folder_path)
-db_name_label2.grid(row=6, column=0)
+open_button.grid(row=8, column=0)
+db_name_label2 = Label(root, text='Folder z plikami nie zostal wybrany')
+db_name_label2.grid(row=10, column=0)
 
-open_button = Button(root, text="Wykonaj", command=lambda: transformExcel())
-open_button.grid(row=7, column=0)
+open_button = Button(root, text="Wykonaj", command=lambda: transform_excel())
+open_button.grid(row=12, column=0)
 
-def openFile():
+error_label = Label(root, text='')
+error_label.grid(row=14, column=0)
+
+
+def open_file():
     global xlsx_path
-    filetype=('excel file','*.xlsx'), ('All files', ('*.*'))
+    filetype=('excel file','*.xlsx;*.xlsm;*.xltx;*.xltm'), ('All files', '*.*')
     filepath=fd.askopenfilenames(filetypes=filetype)
     filepath2 = ''
     for i in filepath:
         filepath2 = filepath2 + i
         xlsx_path = filepath2
-    print(xlsx_path)
     db_name_label1.config(text=xlsx_path)
+
 
 def open_folder():
     global folder_path
@@ -46,28 +52,46 @@ def open_folder():
     folder_path = mr_dir
     db_name_label2.config(text=folder_path)
 
-def transformExcel():
+
+def transform_excel():
     global xlsx_path
     global folder_path
+    now = datetime.datetime.now()
     # Check if the XLSX file path is valid
     if not os.path.isfile(xlsx_path) or not xlsx_path.endswith('.xlsx'):
-        print('Invalid XLSX file path')
-    else:
-        print('XLSX file path:', xlsx_path)
+        with open('errors.txt', 'w') as f:
+            f.write(f'Zly plik Excel\n {now}')
+        error_label.config(text='Zly plik Excel')
+        return
+        
     # Check if the folder path is valid
     if not os.path.isdir(folder_path):
-        print('Invalid folder path')
-    else:
-        print('Folder path:', folder_path)
+        with open('errors.txt', 'w') as f:
+            f.write(f'Zla sciezka folderu z plikami\n {now}')
+        error_label.config(text='Zla sciezka folderu z plikami')
+        return
             
     workbook = load_workbook(xlsx_path)
     worksheet_name = db_name.get()
+    
+    try:
+        worksheet = workbook[worksheet_name]
+    except KeyError:
+        with open('errors.txt', 'w') as f:
+            f.write(f'Zla nazwa arkusza: {worksheet_name} {now}\n')
+        error_label.config(text=f'Zla nazwa arkusza: {worksheet_name}')
+        return
 
-    worksheet = workbook[worksheet_name]
     # Loop through the rows of the worksheet
     for row in worksheet.iter_rows(min_row=2, values_only=True):
         # Get the values from the columns
-        col1, col2, col3, col4 = row
+        try:
+            col1, col2, col3, col4 = row
+        except ValueError:
+            with open('errors.txt', 'w') as f:
+                f.write(f'Zle dane w wierszu: {row}\n {now}')
+            error_label.config(text=f'Zle dane w wierszu: {row}')
+            continue
 
         # Determine the child folder path
         if col2 and col3 and col4:
@@ -86,6 +110,13 @@ def transformExcel():
                 else:
                     dst_filename = col1 + os.path.splitext(filename)[1]
                 dst_path = os.path.join(child_folder, dst_filename)
-                shutil.copy(src_path, dst_path)
-                
+                try:
+                    shutil.copy(src_path, dst_path)
+                    error_label.config(text='Pliki skopiowane poprawnie')
+                except Exception as e:
+                    with open('errors.txt', 'w') as f:
+                        f.write(f'Blad kopiowania pliku {src_path} to {dst_path}: {e} {now}\n')
+                    error_label.config(text=f'Blad kopiowania pliku {src_path} to {dst_path}: {e}')
+
+
 root.mainloop()
